f8a8ff3ca303322e8928b5d9fbee92eccdfe8372
icdrrrrr <icdrrrrr@icdrrrrrs-MacBook-Air.local>
2022-07-08 18:22:29 +0700

    finish frontend design

---
+++ resources/js/Components/RestaurantsList.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <b-list-group>
+    <b-list-group-item  v-for="restaurant in restaurants" :key="restaurant.id"  href="#">
+      <div class="d-flex justify-content-between align-items-center">
+        {{ restaurant.name }}
+        <b-badge variant="primary" pill>{{ restaurant.id }}</b-badge>
+
+      </div>
+      <small>{{ restaurant.description }}</small>
+    </b-list-group-item>
+
+  </b-list-group>
+</template>
+
+<style scoped>
+
+
+.list-group {
+  position: relative;
+  /* height: 450px; */
+  overflow: scroll;
+}
+</style>
+<script>
+export default({
+    props: {
+        restaurants: Array,
+    },
+});
+</script>
--- resources/js/Pages/Main.vue
+++ resources/js/Pages/Main.vue
@@ -1,13 +1,67 @@
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-</script>
-
 <template>
     <AppLayout title="Index">
         <b-row>
-            <b-col cols="4">xxx</b-col>
-            <b-col cols="8">yyy</b-col>
+            <b-col cols="4">
+                <b-form @submit="onSubmit">
+
+                    <b-input-group class="mb-3">
+                        <b-form-input
+                            id="input-1"
+                            type="text"
+                            placeholder="Please enter your location."
+                            required
+                            ></b-form-input>
+                        <b-input-group-append>
+                            <b-button type="submit" size="sm" class="btn-search" text="Search" variant="primary">Search</b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-form>
+                
+
+                <RestaurantsList :restaurants="restaurants" />
+            </b-col>
+            <b-col cols="8">
+                <img src="https://raw.githubusercontent.com/shvam0000/Google-Maps-Clone/main/Mockup%20images/ss-1.PNG" style="width: 100%;" />
+            </b-col>
 
         </b-row>
     </AppLayout>
 </template>
+
+<style scoped>
+.btn-search {
+    border-radius: 0 0.2rem 0.2rem 0;
+}
+</style>
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import RestaurantsList from '@/Components/RestaurantsList.vue';
+ 
+export default {
+    components: {
+        AppLayout, RestaurantsList
+    },
+    data() {
+        return {
+            restaurants: []
+        }
+    },
+    mounted() {
+        this.getall()
+    },
+    methods: {
+        getall: function () {
+            for (let i = 0; i < 10; i++) {
+                this.restaurants.push({
+                    id: i,
+                    name: 'New 1',
+                    lat: 'xxx',
+                    lng: 'yyyy',
+                    description: 'rrrrrrr',
+                    link: 'https://github.com/thdevss'
+                })
+            }
+        }
+    }
+}
+</script>
